efb24076cb23c7976bf7c213fb22ba083ebb3d47
Aditya Palepu <apalepu23@gmail.com>
2015-01-31 02:28:39 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simby::Application.config.secret_key_base = 'e51a6e82b61b6187e4a0276a90765201bb276ef414951dfe1c72b74306cffbe4ba80d2b999fff0bf7b49b19b2f4fd2d62199453762afe9098dbf54173dc6c442'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Simby::Application.config.session_store :cookie_store, key: '_simby_session'
